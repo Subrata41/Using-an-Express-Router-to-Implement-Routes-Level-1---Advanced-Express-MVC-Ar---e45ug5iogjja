54d7f4864abf37a517a274e021910d2370c2a577
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-04-21 23:34:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,53 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-//Aim: With the help of router, get all the product with router.GET request and create a product with router.POST request
+const router = express.Router();
 
-//middleware
-//write router middleware here
-router.use(express.json());
-
-//Including product.json file
+// Including product.json file
 const product = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/product.json`)
 );
 
-// Defining The Router
+// Define middleware
+router.use(express.json());
+
 // Get all the products
 router.get('/api/v1/product', (req, res) => {
   try {
-    //Write your code here
+    res.status(200).json({
+      status: 'success',
+      results: product.length,
+      data: {
+        product,
+      },
+    });
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json({ status: 'error', message: 'Error getting products' });
   }
 });
 
-//Create a new Product
+// Create a new Product
 router.post('/api/v1/product', (req, res) => {
   try {
-    //Write your code here
+    const newProduct = {
+      id: product.length + 1,
+      title: req.body.title,
+      price: req.body.price,
+    };
+    product.push(newProduct);
+    fs.writeFile(`${__dirname}/../dev-data/product.json`, JSON.stringify(product), () => {});
+    res.status(201).json({
+      status: 'success',
+      data: {
+        product: newProduct,
+      },
+    });
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json({ status: 'error', message: 'Error creating product' });
   }
 });
 
-//Registering our Router
-//Write here to register router
+// Registering our Router
+app.use('/', router);
 
 module.exports = app;
